669730e18613fdcecf7ff8fdf0b9cc9fa62e39a2
rockychiang <rocky@chem.ucla.edu>
2018-04-10 17:12:32 -0400

    Added validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :username, presence: true
+  validates :email, confirmation: true
+  validates :email_confirmation, presence: true
+  validates :password, presence: true
 end
