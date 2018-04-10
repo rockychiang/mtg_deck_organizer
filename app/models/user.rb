910054f65b1815691f2c4d86ab696945b437cc3a
rockychiang <rocky@chem.ucla.edu>
2018-04-10 17:49:14 -0400

    Added format and length validation for username, confirmation for email, and length for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :username, presence: true
-  validates :email, confirmation: true
+  validates :username, presence: true,
+    format: {with: /\A[a-zA-Z0-9_-]+\z/, message: "only allow lettters, numbers, dash, and underscore"},
+    length: {minimum: 3, too_short: "must be at least 3 characters long"}
+  validates :email, confirmation: true, uniqueness: true
   validates :email_confirmation, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: {in: 6..20, wrong_length: "cannot be shorter than 6 and longer than 20 characters"}
 end
