aea5d9f0ad02e9c03a08bf02b3365c2c88c14233
rockychiang <rocky@chem.ucla.edu>
2018-04-10 18:43:00 -0400

    Added validations for Card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,6 @@
 class Card < ActiveRecord::Base
-  
+  validates :name, presence: true
+  validates :cost, presence: true,
+    format: {with: /\A[WBRUG0-9]+\z/, message: "please enter white as W, blue as U, red as R, green as G, black as B, and colorless as numbers"}
+  validates :card_type, presence: true
 end
