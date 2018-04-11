57b93685d5bad0eabf7a79bb32b61b0a65f296ef
rockychiang <rocky@chem.ucla.edu>
2018-04-10 21:59:16 -0400

    Added quantity method to card model and editted the deck show page to show the quantity of each card

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,4 +5,8 @@ class Card < ActiveRecord::Base
   validates :cost, presence: true,
     format: {with: /\A[WBRUG0-9]+\z/, message: "please enter white as W, blue as U, red as R, green as G, black as B, and colorless as numbers"}
   validates :card_type, presence: true
+
+  def quantity(deck_id)
+    self.deck_cards.where(deck_id: deck_id, card_id: self.id).first.quantity
+  end
 end
