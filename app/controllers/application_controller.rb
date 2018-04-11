422c7f6dbbfee52d6b67ad8a25d4da3460b9d0a6
rockychiang <rocky@chem.ucla.edu>
2018-04-10 20:16:39 -0400

    added helpers methods and logout route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,11 @@ class ApplicationController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
-    erb :index
+    if logged_in?
+      redirect '/decks'
+    else
+      erb :index
+    end
   end
 
   post '/login' do
@@ -16,6 +20,22 @@ class ApplicationController < Sinatra::Base
     else
       redirect '/'
     end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
   end
 
 end
