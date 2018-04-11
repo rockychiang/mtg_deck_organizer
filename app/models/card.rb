f8631bfbdcb407f312c68cbf96386d5f6da08e36
rockychiang <rocky@chem.ucla.edu>
2018-04-11 03:55:55 -0400

    Finished coding the deck parser method

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,7 +3,7 @@ class Card < ActiveRecord::Base
   has_many :decks, through: :deck_cards
   validates :name, presence: true
   validates :cost, presence: true,
-    format: {with: /\A[WBRUG0-9]+\z/, message: "please enter white as W, blue as U, red as R, green as G, black as B, and colorless as numbers"}
+    format: {with: /\A[WBRUG0-9{}]+\z/, message: "please enter white as {W}, blue as {U}, red as {R}, green as {G}, black as {B}, and colorless as {#}"}
   validates :card_type, presence: true
 
   def quantity(deck_id)
--- app/models/helper/deck_parser.rb
+++ app/models/helper/deck_parser.rb
@@ -1,11 +1,27 @@
 class DeckParser
 
   def self.parse(deck, deck_list)
-    cards = deck_list.split("\n")
-    cards.each do |card|
-      card.strip!
-      name = card.sub(/\A[0-9]+x\s/, "").strip
-      quantity = card.match(/\A[0-9]+/).strip.to_i
+    list = deck_list.split("\n")
+    list.each do |line|
+      line.strip!
+      name = line.sub(/\A[0-9]+x\s/, "").strip
+      db_card = MTG::Card.where(name: name).all.first
+      card = Card.find_or_create_by(name: db_card.name)
+      if !card.id
+        card.cost = db_card.mana_cost
+        card.card_type = db_card.types.join(" ")
+        card.card_subtype = db_card.subtypes.join(" ") if !!db_card.subtypes
+        card.card_text = db_card.text if !!db_card.text
+        if db_card.types.include?("Creature")
+          card.power = db_card.power
+          card.toughness = db_card.toughness
+        end
+        card.save
+      end
+      quantity = line.match(/\A[0-9]+/).strip.to_i
+      deck_cards = DeckCard.find_or_create_by(deck_id: deck.id, card_id: card.id)
+      deck_cards.quantity = quantity
+      deck_cards.save
     end
   end
 
