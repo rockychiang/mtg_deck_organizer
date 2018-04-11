e5b4bfc9f05dbb7e7b271ee9a1551240e77fccef
rockychiang <rocky@chem.ucla.edu>
2018-04-11 19:58:18 -0400

    Refactor DeckParser to be more readible

--- app/models/helper/deck_parser.rb
+++ app/models/helper/deck_parser.rb
@@ -1,7 +1,18 @@
 class DeckParser
 
   def self.run(deck, deck_list)
-    card_list = deck_list.strip.split("\n").map do |line|
+    card_list = parse_deck(deck_list)
+    if !card_list.include?("error")
+      update_deck(deck, card_list)
+    else
+      raise "error(s) in deck list"
+    end
+  end
+
+  private
+
+  def self.parse_deck(deck_list)
+    deck_list.strip.split("\n").map do |line|
       line.strip!
 
       name = line.sub(/\A[0-9]+x\s/, "").strip
@@ -14,20 +25,14 @@ class DeckParser
         "error"
       end
     end
-
-    if !card_list.include?("error")
-      deck.cards.clear
-      card_list.each do |card|
-        deck.cards << card[0]
-        deck.deck_cards.find_by(card_id: card[0].id).update(quantity: card[1])
-      end
-    else
-      raise "error in deck list"
-    end
   end
 
-  private
-
-
+  def self.update_deck(deck, card_list)
+    deck.cards.clear
+    card_list.each do |card|
+      deck.cards << card[0]
+      deck.deck_cards.find_by(card_id: card[0].id).update(quantity: card[1])
+    end
+  end
 
 end
