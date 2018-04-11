a16f745ab38598f7ff2ff3ea52ac7cdd9b0f113a
rockychiang <rocky@chem.ucla.edu>
2018-04-11 19:15:13 -0400

    Refactor some code from DeckParser into card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -9,4 +9,33 @@ class Card < ActiveRecord::Base
   def quantity(deck_id)
     self.deck_cards.where(deck_id: deck_id, card_id: self.id).first.quantity
   end
+
+  def update_from_sdk
+    if !!self.name
+      db_card = MTG::Card.where(name: self.name).all.first
+      if !!db_card
+        self.name = db_card.name
+        self.cost = db_card.mana_cost
+        self.card_type = db_card.types.join(" ")
+        # card.card_supertype = db_card.supertypes.join(" ") if !!db_card.supertypes
+        self.card_subtype = db_card.subtypes.join(" ") if !!db_card.subtypes
+        self.card_text = db_card.text
+        self.power = db_card.power
+        self.toughness = db_card.toughness
+        self.save
+      else
+        raise "No card named #{self.name} found in SDK"
+      end
+    else
+      raise "Can't update card without a name"
+    end
+  end
+
+  def self.build_from_sdk(name: card_name)
+    Card.new.tap do |card|
+      card.name = name
+      card.update_from_sdk
+    end
+  end
+
 end
--- app/models/helper/deck_parser.rb
+++ app/models/helper/deck_parser.rb
@@ -1,34 +1,40 @@
 class DeckParser
 
   def self.run(deck, deck_list)
-    list = deck_list.strip.split("\n")
-    list.each do |line|
+    card_list = deck_list.strip.split("\n").map do |line|
       line.strip!
 
       name = line.sub(/\A[0-9]+x\s/, "").strip
-      quantity = line.match(/\A[0-9]+/)[0].strip.to_i
+      quantity = line.to_i
+      card = Card.where("name like ?", name).first || get_from_sdk(name)
 
-      card = Card.where("name like ?", name).first || assign_attribute(name)
-
-      deck_cards = DeckCard.find_or_create_by(deck_id: deck.id, card_id: card.id)
-      deck_cards.quantity = quantity
-      deck_cards.save
+      if card && quantity > 0
+        [card, quantity]
+      else
+        "error"
+      end
     end
+    deck_cards = DeckCard.find_or_create_by(deck_id: deck.id, card_id: card.id)
+    deck_cards.quantity = quantity
+    deck_cards.save
   end
 
   private
 
-  def assign_attribute(name)
-    db_card = MTG::Card.where(name: name).all.first
-    Card.find_or_create_by(name: db_name).tap do |card|
-      card.cost = db_card.mana_cost
-      card.card_type = db_card.types.join(" ")
-      # card.card_supertype = db_card.supertypes.join(" ") if !!db_card.supertypes
-      card.card_subtype = db_card.subtypes.join(" ") if !!db_card.subtypes
-      card.card_text = db_card.text
-      card.power = db_card.power
-      card.toughness = db_card.toughness
-      card.save
+  def get_from_sdk(name)
+    if db_card = MTG::Card.where(name: name).all.first
+      Card.find_or_create_by(name: db_name).tap do |card|
+        card.cost = db_card.mana_cost
+        card.card_type = db_card.types.join(" ")
+        # card.card_supertype = db_card.supertypes.join(" ") if !!db_card.supertypes
+        card.card_subtype = db_card.subtypes.join(" ") if !!db_card.subtypes
+        card.card_text = db_card.text
+        card.power = db_card.power
+        card.toughness = db_card.toughness
+        card.save
+      end
+    else
+      nil
     end
   end
 
