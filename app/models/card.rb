23b26d94696a6170ef859f7f4418edfb66aff0d2
rockychiang <rocky@chem.ucla.edu>
2018-04-11 20:07:29 -0400

    Editted mana cost validation for card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,8 +2,6 @@ class Card < ActiveRecord::Base
   has_many :deck_cards
   has_many :decks, through: :deck_cards
   validates :name, presence: true
-  validates :cost, presence: true,
-    format: {with: /\A[WBRUG0-9{}]+\z/, message: "please enter white as {W}, blue as {U}, red as {R}, green as {G}, black as {B}, and colorless as {#}"}
   validates :card_type, presence: true
 
   def quantity(deck_id)
