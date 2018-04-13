aaa51bd1adb3b3c40cdc84209b1152c182ca0fa7
rockychiang <rocky@chem.ucla.edu>
2018-04-13 02:02:11 -0400

    Debugged deck parser method to ignore any blank lines

--- app/models/helper/deck_parser.rb
+++ app/models/helper/deck_parser.rb
@@ -14,22 +14,25 @@ class DeckParser
   def self.parse_deck(deck_list)
     deck_list.strip.split("\n").map do |line|
       line.strip!
+      if !line.empty?
+        name = line.sub(/\A[0-9]+x\s/, "").strip
+        quantity = line.to_i
+        card = Card.where("name like ?", name).first || Card.build_from_sdk(name: name)
 
-      name = line.sub(/\A[0-9]+x\s/, "").strip
-      quantity = line.to_i
-      card = Card.where("name like ?", name).first || Card.build_from_sdk(name: name)
-
-      if card.id && quantity > 0
-        [card, quantity]
+        if card.id && quantity > 0
+          [card, quantity]
+        else
+          "error"
+        end
       else
-        "error"
+        nil
       end
     end
   end
 
   def self.update_deck(deck, card_list)
     deck.cards.clear
-    card_list.each do |card|
+    card_list.compact.each do |card|
       deck.cards << card[0]
       deck.save
       deck.deck_cards.find_by(card_id: card[0].id).update(quantity: card[1])
